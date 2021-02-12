588660037b5574328b0b88cd7f0dddc22ba4e2e1
Max Orok <maxwellorok@gmail.com>
2021-02-12 10:43:26 -0500

    Disambiguate nix socket module

--- src/net/tcp/mod.rs
+++ src/net/tcp/mod.rs
@@ -10,22 +10,22 @@ use std::io;
 use std::net::{SocketAddr, ToSocketAddrs};
 use std::os::unix::io::RawFd;
 
-use nix::sys::socket as nix;
+use nix::sys::socket as nix_socket;
 
-fn socket<A: ToSocketAddrs>(addr: A, protocol: nix::SockProtocol) -> io::Result<(RawFd, SocketAddr)> {
+fn socket<A: ToSocketAddrs>(addr: A, protocol: nix_socket::SockProtocol) -> io::Result<(RawFd, SocketAddr)> {
     use io::{Error, ErrorKind};
 
     let mut error = Error::new(ErrorKind::InvalidInput, "could not resolve to any addresses");
 
     for addr in addr.to_socket_addrs()? {
         let domain = match addr.is_ipv6() {
-            true    => nix::AddressFamily::Inet6,
-            false   => nix::AddressFamily::Inet,
+            true    => nix_socket::AddressFamily::Inet6,
+            false   => nix_socket::AddressFamily::Inet,
         };
 
-        let flags = nix::SockFlag::SOCK_CLOEXEC;
+        let flags = nix_socket::SockFlag::SOCK_CLOEXEC;
 
-        match nix::socket(domain, nix::SockType::Stream, flags, Some(protocol)) {
+        match nix_socket::socket(domain, nix_socket::SockType::Stream, flags, Some(protocol)) {
             Ok(fd)          => return Ok((fd, addr)),
             _               => error = io::Error::last_os_error(),
         }
--- src/unix/mod.rs
+++ src/unix/mod.rs
@@ -7,17 +7,20 @@ mod stream;
 pub use listener::{UnixListener, Close, Accept, Incoming};
 pub use stream::{UnixStream, Connect};
 
-use nix::sys::socket as nix;
+use nix::sys::socket as nix_socket;
 
 fn socket() -> io::Result<RawFd> {
-    match nix::socket(nix::AddressFamily::Unix, nix::SockType::Stream, nix::SockFlag::SOCK_CLOEXEC, None) {
+    match nix_socket::socket(nix_socket::AddressFamily::Unix,
+        nix_socket::SockType::Stream, nix_socket::SockFlag::SOCK_CLOEXEC, None)
+    {
         Ok(fd)  => Ok(fd),
         Err(_)  => Err(io::Error::last_os_error()),
     }
 }
 
 fn socketpair() -> io::Result<(RawFd, RawFd)> {
-    match nix::socketpair(nix::AddressFamily::Unix, nix::SockType::Stream, None, nix::SockFlag::SOCK_CLOEXEC) {
+    match nix_socket::socketpair(nix_socket::AddressFamily::Unix,
+        nix_socket::SockType::Stream, None, nix_socket::SockFlag::SOCK_CLOEXEC) {
         Ok((fd1, fd2))  => Ok((fd1, fd2)),
         Err(_)          => Err(io::Error::last_os_error()),
     }
